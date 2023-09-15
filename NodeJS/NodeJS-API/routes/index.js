5341ade8abbbf9971451a35621bb8f052f18a1b6
Ninei <seeninei@gmail.com>
2023-09-15 17:55:22 +0900

    Point post

--- NodeJS/NodeJS-API/routes/index.js
+++ NodeJS/NodeJS-API/routes/index.js
@@ -6,15 +6,4 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'HI~~! Express' });
 });
 
-// router.post('/point/save/:type', async(req, res)=>{
-//   let {type} = req.params;
-//   let userData = req.body.user;
-
-//   console.log(type);
-//   console.log(userData.sn);
-//   console.log(userData.point);
-
-//   res.send("ok\ntype: " + type + ", sn: " + userData.sn + ", point: " + userData.point);
-// });
-
 module.exports = router;
